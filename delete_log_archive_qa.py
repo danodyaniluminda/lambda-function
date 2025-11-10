d7a7a88f32b71a8a79ec99a332a0e51ca5a845a7
Danodya Senewirathne <44151190+danodyaniluminda@users.noreply.github.com>
2025-11-10 15:06:45 +0530

    change python code

--- delete_log_archive_qa.py
+++ delete_log_archive_qa.py
@@ -1,17 +1,16 @@
 import os
 import datetime
 
-efs_mount_path = "/DTE_app_logs"
-days_threshold_delete = 7
+efs_mount_path = "/mnt/efs"
+days_threshold_delete = 90
 
 def lambda_handler(event, context):
     try:
-        for entry in os.scandir(efs_mount_path):
-            if entry.is_dir() and entry.name == "logs":
-                process_directory(entry.path)
+        process_directory(efs_mount_path)
+
         return {
             'statusCode': 200,
-            'body': 'Old log and gz files deleted successfully.'
+            'body': 'Old gz files deleted successfully.'
         }
     except Exception as e:
         return {
@@ -30,7 +29,7 @@ def delete_old_file(file_path):
 
 def process_directory(directory_path):
     for entry in os.scandir(directory_path):
-        if entry.is_file() and (entry.name.endswith('.gz') or entry.name.endswith('.log')):
+        if entry.is_file() and entry.name.endswith('.gz'):
             if calculate_time_difference(entry.path) >= days_threshold_delete:
                 delete_old_file(entry.path)
         elif entry.is_dir():
